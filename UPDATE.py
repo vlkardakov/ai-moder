17f38494b426bc50097c4b886df88d7ef1481202
vlkardakov <170172399+vlkardakov@users.noreply.github.com>
2025-01-11 01:58:03 +0300

    Update UPDATE.py

--- UPDATE.py
+++ UPDATE.py
@@ -1,8 +1,23 @@
 import pyautogui as pg
 
+def window_up(window_title):
+  try:
+    # Получаем список всех открытых окон, содержащих заданный заголовок.
+    windows = pg.getWindowsWithTitle(window_title)
+
+    if windows:
+      # Берем первое окно из списка (обычно это нужное окно).
+      window = windows[0]
+      window.activate()
+      print(f"Окно с заголовком '{window_title}' активировано.")
+    else:
+      print(f"Окно с заголовком '{window_title}' не найдено.")
+  except Exception as e:
+    print(f"Произошла ошибка: {e}")
 
 def github_update():
     pg.click(750, 80)
 
 if __name__ == "__main__":
+    window_up("GitHub Desktop")
     github_update()
