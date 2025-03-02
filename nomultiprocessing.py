662081837a9664d4bdf2b417063c80c239bd5569
vlkardakov <170172399+vlkardakov@users.noreply.github.com>
2025-03-02 12:21:01 +0300

    'g'

--- nomultiprocessing.py
+++ nomultiprocessing.py
@@ -4,12 +4,18 @@ from selenium.webdriver.chrome.service import Service
 from urllib.parse import urlparse
 import os
 from concurrent.futures import ThreadPoolExecutor
+import tempfile
+
 
 def get_domain(url):
     return urlparse(url).netloc
 
+
 def normal_filename(link):
-    return "screenshots/" + link.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace("?", '').replace('=','').replace('%','') + ".png"
+    return "screenshots/" + link.replace("https://", "").replace("http://", "").replace("/", "_").replace(":",
+                                                                                                          "_").replace(
+        "?", "").replace("=", "").replace("%", "") + ".png"
+
 
 def process_link(link):
     try:
@@ -22,13 +28,14 @@ def process_link(link):
         # Путь к исполняемому файлу Chromium
         options.binary_location = '/usr/bin/chromium-browser'
 
+        # Уникальная директория для профиля
+        temp_dir = tempfile.mkdtemp()
+        options.add_argument(f"--user-data-dir={temp_dir}")
+
         # Путь к драйверу Chromium
         driver_path = '/usr/lib/chromium-browser/chromedriver'
 
-        # Создаем объект Service
         service = Service(executable_path=driver_path)
-
-        # Создаем экземпляр WebDriver для Chromium
         driver = webdriver.Chrome(service=service, options=options)
 
         output = normal_filename(link)
@@ -41,21 +48,21 @@ def process_link(link):
         driver.quit()
 
         print(f"Скриншот сохранен как {output}")
-
         return {"url": link, "title": title, "screenshot": output, "domain": get_domain(link)}
     except Exception as e:
         print(f"Ошибка при обработке {link}: {e}")
         return None
+
 
 def process_links(links):
     with ThreadPoolExecutor(max_workers=4) as executor:  # 4 потока для ускорения
         results = list(executor.map(process_link, links))
     return results
 
+
 if __name__ == "__main__":
     links = ["https://google.com", "https://amazon.com", "https://yandex.ru", "https://minilink.pro"]
-
     data = process_links(links)
     for el in data:
-        if el:  # Проверяем на None
+        if el:
             print(el)
