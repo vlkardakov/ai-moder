91d46fb5fdff191a9737c22d8eb4d37dbdfa2586
vlkardakov <170172399+vlkardakov@users.noreply.github.com>
2025-03-02 12:50:16 +0300

    'g'

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 
 options = Options()
-driver = webdriver.Firefox(options=options)
+service = Service('venv/bin/geckodriver')
+
+driver = webdriver.Firefox(service=service, options=options)
+
 driver.get("https://www.example.com")
 print(driver.title)
 driver.quit()
