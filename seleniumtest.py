c008dad2df1308711ce58f36a1745baa87808184
vlkardakov <170172399+vlkardakov@users.noreply.github.com>
2025-03-02 12:51:16 +0300

    'g'

--- seleniumtest.py
+++ seleniumtest.py
@@ -3,7 +3,17 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
 options = Options()
+options.add_argument("--log-level=3")
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--headless")
+options.add_argument("--start-maximized")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--incognito")
+
+
 service = Service('venv/bin/geckodriver')
+
 
 driver = webdriver.Firefox(service=service, options=options)
 
