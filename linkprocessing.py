74e7d8325def350da50977eeb3293af6f0b26871
vlkardakov <170172399+vlkardakov@users.noreply.github.com>
2025-03-02 10:57:47 +0300

    'g'

--- linkprocessing.py
+++ linkprocessing.py
@@ -8,36 +8,45 @@ def get_domain(url):
     return urlparse(url).netloc
 
 def normal_filename(link):
-    return "screenshots/" + link.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace("?", "").replace("=", "").replace("%", "") + ".png"
+    return "screenshots/" + link.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace("?", '').replace('=','').replace('%','') + ".png"
 
 def process_link(link):
-    options = Options()
-    options.binary = '/snap/bin/firefox'  # явно указываем путь к Firefox из snap
-    options.add_argument("--log-level=3")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--headless")
-    options.add_argument("--start-maximized")
+    # try:
+        # options = webdriver.ChromeOptions()
+        options = Options()
+        options.add_argument("--log-level=3")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--headless")
+        options.add_argument("--start-maximized")
 
-    driver = webdriver.Firefox(options=options)
-    output = normal_filename(link)
-    if os.path.exists(output):
-        os.remove(output)
+        # driver = webdriver.Chrome(options=options)
 
-    driver.get(link)
-    title = driver.title
-    driver.save_screenshot(output)
-    driver.quit()
 
-    print(f"Скриншот сохранен как {output}")
-    return {"url": link, "title": title, "screenshot": output, "domain": get_domain(link)}
+        driver = webdriver.Firefox(options=options)
 
+        output = normal_filename(link)
+        # pass
+        if os.path.exists(output):
+            os.remove(output)
+
+        driver.get(link)
+        title = driver.title
+        driver.save_screenshot(output)
+        driver.quit()
+
+        print(f"Скриншот сохранен как {output}")
+
+        return {"url": link, "title": title, "screenshot": output, "domain": get_domain(link)}
+    # except:
+    #     return None
 def process_links(links):
-    with ThreadPoolExecutor(max_workers=4) as executor:
+    with ThreadPoolExecutor(max_workers=4) as executor:  # 4 потока для ускорения
         results = list(executor.map(process_link, links))
     return results
 
 if __name__ == "__main__":
     links = ["https://google.com", "https://amazon.com", "https://yandex.ru", "https://minilink.pro"]
+
     data = process_links(links)
     for el in data:
         print(el)
